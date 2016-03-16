5072d3f269923eb0db4ab733ea564d7948d3bda2
Doh <tank2705@gmail.com>
2016-03-16 07:47:18 +0000

    Finished refactoring

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,29 @@
 module Listable
   # Listable methods go here
+  def format_description
+    "#{@description}".ljust(25)
+  end
+  
+  def format_date
+    if self.class == EventItem
+      dates = @start_date.strftime("%D") if @start_date
+      dates << " -- " + @end_date.strftime("%D") if @end_date
+      dates = "N/A" if !dates
+      return dates
+    elsif self.class == TodoItem
+      @due ? @due.strftime("%D") : "No due date"
+    else
+      "Null"
+    end
+  end
+  
+  
+  def format_priority
+    value = " ⇧" if @priority == "high"
+    value = " ⇨" if @priority == "medium"
+    value = " ⇩" if @priority == "low"
+    value = "" if !@priority
+    return value
+  end
+  
 end
